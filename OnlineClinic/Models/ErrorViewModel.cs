15908cad3f0a808cc55463172f1095d620686e5a
lifeismeme <59262137+lifeismeme@users.noreply.github.com>
2020-09-03 14:10:38 +0800

    Add project files.

---
+++ OnlineClinic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineClinic.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
